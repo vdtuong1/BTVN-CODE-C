77208ac813173c5f1444b846014a581d19d158f2
vdtuong1 <147390887+vdtuong1@users.noreply.github.com>
2024-11-23 18:47:47 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // khai bao va khoi tao cac bien 
+
+    int a = 10;          
+    float b = 3.14f;     
+    double c = 3.14159;  
+    char d = 'A';        
+    short e = 5;        
+    long f = 100000L;   
+    
+   // in ra gtri cua bien 
+    printf("a = %d\n", a);  
+    printf("b = %f\n", b);  
+    printf("c = %lf\n", c); 
+    printf("d = %c\n", d);  
+    printf("e = %d\n", e);  
+    printf("f = %ld\n", f); 
+    printf("g = %u\n", g);  
+
+    return 0;
+}
+
